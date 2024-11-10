909c9761d8b4966d0aaa0b2abc0aed49ead4681b
Viet <110122858+Viet170916@users.noreply.github.com>
2024-11-10 22:45:17 +0700

    modify product v2

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,11 +1,35 @@
 import prisma from "@/app/libs/Prisma";
-import { NextResponse } from "next/server";
-export async function GET(req){
-    try {
+import {NextResponse} from "next/server";
+import {bucketName, s3_v2} from "@/AWS";
+import {GetObjectCommand} from "@aws-sdk/client-s3";
+import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
 
+export async function GET(){
+    // let productsResponse=[];
+    try {
         const product = await prisma.products.findMany({})
+        // productsResponse = product;
+        for (let i = 0; i< product.length; i++) {
+            const params = {
+                Bucket: bucketName,
+                Key: product[i].url,
+                // ContentType: post.type,
+            };
+            const command = new GetObjectCommand(params);
+            product[i].url = await getSignedUrl(s3_v2, command, {expiresIn: 3600});
+        }
+        // const params = {
+        //     Bucket: bucketName,
+        //     Key: product[0].url,
+        //     // ContentType: post.type,
+        // };
+        // const command = new GetObjectCommand(params);
+        //
+        // product[0].url = await getSignedUrl(s3_v2, command, {expiresIn: 3600});
         await prisma.$disconnect();
         return NextResponse.json(product);
+        // return NextResponse.json({url: url});
+        
         
     } catch (error) {
         console.log('error',error);
--- src/app/components/Product.js
+++ src/app/components/Product.js
@@ -7,15 +7,15 @@ export default function Product({product}) {
         <Link href={`/product/${product.id}`} className={`max-w-[300px] p-1.5 border border-gray-50 hover:border-gray-200 hover:shadow-xl bg-gray-100 rounded mx-auto`}>
             {
                 product?.url ? (
-                    <Image width={300} height={300} className="rounded cursor-pointer " alt="product-img" src={product.url + '/280'}/>
+                    <Image width={300} height={300} className="rounded cursor-pointer " alt="product-img" src={product.url}/>
                 )
                 : null
             }
             <div className="pt-2 px-1">
                 <div className="font-semibold text-[15px] hover:underline cursor-pointer">{product?.title}</div>
-                <div className="font-extrabold">₹{(product?.price / 100).toFixed(2)}</div>
+                <div className="font-extrabold">${(product?.price / 100).toFixed(2)}</div>
                 <div className="relative flex items-center text-[12px] text-gray-500">
-                    <div className="line-through">₹{((product?.price * 1.2) / 100).toFixed(2)}</div>
+                    <div className="line-through">${((product?.price * 1.2) / 100).toFixed(2)}</div>
                     <div className="px-2">-</div>
                     <div className="line-through">20%</div>
                 </div>
